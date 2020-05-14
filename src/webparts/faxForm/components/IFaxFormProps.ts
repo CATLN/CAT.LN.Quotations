d2a61378a99d866e5bc87ee55b760afdda5e2ea9
RavikiranBethi <Ravikiran.Bethi@yash.com>
2020-05-14 17:23:17 +0530

    Added FaxForm Webpart By Ravikiran

---
+++ src/webparts/faxForm/components/IFaxFormProps.ts
@@ -0,0 +1,3 @@
+export interface IFaxFormProps {
+  description: string;
+}
---
+++ src/webparts/faxForm/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFaxFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FaxFormWebPartStrings' {
+  const strings: IFaxFormWebPartStrings;
+  export = strings;
+}
