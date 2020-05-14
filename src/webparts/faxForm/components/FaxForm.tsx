13daf17649bdaa3c2a48ba0def408d872b3aece0
Vinay Narasapurapu <vinay@sharepointkids.onmicrosoft.com>
2020-05-14 17:32:28 +0530

    update test to fax form

--- src/webparts/faxForm/components/FaxForm.tsx
+++ src/webparts/faxForm/components/FaxForm.tsx
@@ -10,7 +10,7 @@ export default class FaxForm extends React.Component<IFaxFormProps, {}> {
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <span className={ styles.title }>Welcome to Fax Form!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
